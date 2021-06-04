6d96a7ed1e3c92fb6c9ff19939cf743488589b61
CarloD39 <carlodurand@hotmail.it>
2021-06-04 17:12:23 +0200

    snack12

---
+++ Snack-12/js/script.js
@@ -0,0 +1,40 @@
+// SNACK 12 Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+var Names = ['Carlo', 'Lugi', 'Pietro', 'Temistocle', 'Alessandro', 'Enea'];
+
+var LastNames = ['Blu', 'Verde', 'Rosso', 'Arancio', 'Nero', 'Bianchi'];
+
+var RandomList = [''];
+
+var NamesLe = (Names.length -1);
+
+// Ciclo For//
+for(var i = 0; i < 3; i++){
+
+    var RandomNames = Names[Math.floor(Math.random() * Names.length)];
+    var RandLastNames = LastNames[Math.floor(Math.random() * LastNames.length)];
+
+    var NewGuest = RandomNames + '  ' + RandLastNames;
+
+    RandomList.includes(NewGuest);
+
+    RandomList.push(NewGuest);
+
+    document.getElementById('stampa').innerHTML += "<li>" + NewGuest + "</li>";
+}
+
+// Ciclo While//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
